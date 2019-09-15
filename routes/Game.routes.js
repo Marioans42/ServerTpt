53d77a12f5b525d683a9a74d8a9faaf88ea403ff
hobs <hobianarazakanaivo@gmail.com>
2019-09-15 23:13:10 +0300

    add routes list tech, tags platforms

--- routes/Game.routes.js
+++ routes/Game.routes.js
@@ -1,6 +1,10 @@
 const controller = require('../controller/game.controller')
 const router = require('express').Router();
 
+
+router.get('/platforms', controller.getPlatforms);
+router.get('/technologies', controller.getTechnologies);
+router.get('/tags', controller.getTags);
 router.post('/newGame', controller.createGame);
 router.get('/allGame', controller.allGame);
 router.get('/:id', controller.getGame);
